b5a97be9bdeda07c4aeffa934d751d3f66f9b31e
simonkhartoum <simonkhartoum@gmail.com>
2025-05-30 21:14:24 +0300

    inputs & outputs

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner; //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);  // Create Scanner object
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();  // Read user input as a string
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();  // Read user input as an integer
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+
+        scanner.close();
     }
 }
