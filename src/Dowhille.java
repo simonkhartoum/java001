6f7b06aac8ea432d16f334b9b3515fce80461cb4
simonkhartoum <simonkhartoum@gmail.com>
2025-05-31 12:27:57 +0300

    for,while, do while loops

---
+++ src/Dowhille.java
@@ -0,0 +1,9 @@
+public class Dowhille {
+    public static void main(String[]args){
+        int j= 0;
+        do {
+            System.out.println(j);
+            j++;
+        } while(j <= 5);
+    }
+}
